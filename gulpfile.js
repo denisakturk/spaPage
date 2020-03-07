b68eb7c8f2889710e308a924244cd29c09e31bd6
Denis Akturk <denisakturk@yandex.ru>
2020-03-07 11:07:09 +0300

    carousel #2

--- gulpfile.js
+++ gulpfile.js
@@ -31,7 +31,7 @@ browserSync({ // Выполняем browserSync
 gulp.task('scripts', function() {
 return gulp.src([ // Берем все необходимые библиотеки
     'app/libs/jquery/dist/jquery.min.js', // Берем jQuery
-    'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js' // Берем Magnific Popup
+    'app/libs/owl.carousel/dist/owl.carousel.js' // Берем owl carousel
     ])
     .pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
     .pipe(uglify()) // Сжимаем JS файл
