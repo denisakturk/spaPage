e4f7c754c5479b0625c7c1e96170d2c94793d98e
Denis Akturk <denisakturk@yandex.ru>
2020-03-08 03:23:55 +0300

    fix carousel testimonials + services carousel mouseon mouseout

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,9 +1,33 @@
 $(document).ready(function(){
- 	$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    items: 1,
-    autoplay: true,
+
+	$('.carousel-one').owlCarousel({
+		margin:5,
+		loop: true,
+		rewind: true,
+		responsive: {
+			0: {
+				items: 1,
+			},
+			768: {
+				items:3,
+			},
+			991: {
+				items: 4,
+			}
+		},
+	});
+
+ 	$('.carousel-two').owlCarousel({
+	   loop:true,
+	   margin:10,
+	   nav:true,
+	   items: 1,
+	   autoplay: true,
+	});
+
+	$('.services__item').hover(function() {
+			$(this).find(".services__item-onhover").css("visibility", "visible").css("opacity", "100%");
+	}, function() {
+			$(this).find(".services__item-onhover").css("visibility", "hidden").css("opacity", "0");
 	});
 });
